b0a365a41c29d9520b84ee6204c428553824c35b
Noah Kiani <noah.kiani@hva.nl>
2025-11-18 18:36:22 +0100

    fixed_les3_file

---
+++ homework/Week_3/Les 3.py
@@ -0,0 +1,119 @@
+# -*- coding: utf-8 -*-
+"""
+
+@ Adaptaed to Python by Alejandro Murrieta-Mendoza
+
+INTRODUCTION:
+
+    THIS IS A TOY PROBLEM DESIGNED FOR YOUR BRAIN.
+    TO DEVELOP CREATIVIY AND CRITICAL THINKING.
+    PLEASE TRY NOT TO USE AI. IT DEFEATS THE PURPOSE.
+
+
+In this workshop, you will optimize a flight trajectory using two different
+methods. First, analyze the following code for three different trajectories
+(left, middle and right). You can also find distance factors, which are
+randomized factors that simulates the effect of the wind on the aircraft's speed.
+For example, a distance factor of 1.1 would mean that the distance between
+point A and point B is increased by 10%, simulating headwind.
+A distance factor of 0.9 simulates tailwinds thus the distance is reduced.
+
+
+This is your DATA (See Figure 1 in DLO to understand this):
+    ALL IN DEGREES
+lon_lef = [4.76, 4.87, 3.69, 2.36, 2.74, 2.88, 2.69]
+lat_lef = [52.30, 52.05, 50.31, 48.41, 46.09, 43.82, 41.88]
+lon_mid = [4.76, 4.79, 3.62, 2.287, 2.67, 2.81, 2.69]
+lat_mid = [52.30, 52.04, 50.30, 48.39, 46.09, 43.81, 41.88]
+lon_rig = [4.76, 4.72, 3.55, 2.21, 2.59, 2.73, 2.69]
+lat_rig = [52.30, 52.04, 50.30, 48.39, 46.09, 43.81, 41.88]
+
+    NO UNITS (Wind Factor)
+midDF = [1.1, 1.2, 1, 0.96, 0.99, 0.98, 0.95]
+lefDF = [1.1, 0.95, 0.96, 0.99, 1.2, 1.2, 0.95]
+rigDF = [1.1, 0.9, 0.97, 1, 1.2, 1.2, 0.95]
+
+lon_mid and lat_mid are the coordinates for the reference trajectory
+lon_rig and lat rit are the coordinates for the trajectory at the right
+midDF, leftDF, and rig DF are the distance factores (wind emulation)
+
+
+"""
+
+"""
+1.- Place the vinc.py file (wotkshop 1) in the same directory as this file. 
+Then load the v_direct and v_inverse functions. 
+"""
+
+"""
+
+2.- Download the function vinc.py from brightspace and place it in the SAME
+directory as where this file is placed. You also need to import file from
+function. What do yo think this file is doing?. Feel free to open it
+and to take a look at it. 
+
+
+
+3.- Observe how the function is used    """
+# boston = (lat,long) - Longitudes west of Greenwish are negative.
+
+from vinc import v_direct
+
+boston = (42.3541165, -71.0693514)
+newyork = (40.7791472, -73.9680804)
+x = v_direct(boston, newyork)  # In m and degrees
+print("The distance between Boston and New York is: " + str(x[0]) + " m.")
+print("The initial azimuth  between Boston and New York is: " + str(x[1]) + " degrees")
+
+"""
+3.- Find the coordinates of Schipol, Los Angeles (LAX), and Narita (NAA) in degrees. 
+"""
+# ams_lat = ____
+# ams_lon = ____
+# AMS = (ams_lat, ams_lon)  # respect the order
+
+# naa_lat = ____
+# naa_lon = ____
+# NAA = (____, ____)
+
+# lalaland_lat = _____
+# lalaland_lon = _____
+# LAX = _____
+"""
+4.- Use the imported function v_direct to find the distances between AMS - LAX, 
+     AMS - NAA, and LAX-NAA in meters, kilometers and nautical miles. 
+     Print the results
+"""
+"""
+
+5.- Using the imported functions. Place 4 waypoints located 150 nautical miles
+    West, East, North, and South from Schiphol. Plot the waypoints. 
+"""
+# CODE
+"""
+6.- Load the data shown in the introduction  (the 9 lists above)
+"""
+# CODE
+"""
+7.- Using a For loop, calculate the total distance, including the distance
+    factors, for each one of the trajectories. Use the average of distance
+    factors between point A and point B. Example, if the aircraft is at the
+    first waypoint of the middle trajectory, and it flies to the second
+    waypoint of the middle trajectory, the distance factor to be used
+    would be (1.1+1.2)/2 (see data above). 
+    Which one is the shortest trajectory?
+    Print the result in km and in nautical miles
+
+"""
+# CODE
+
+
+"""
+8.- In this step, a simple greedy optimization algorithm should be applied.
+    Try to optimize your distance by calculating at each waypoint, the
+    shortest path. The aircraft should calculate the costs of going to either
+    one of the options (left, middle or right), and fly to the least expensive.
+    This process should be repeated until the aircraft reaches the last waypoint.
+    Show this trajectory on a map. You can move along mid, left or right. 
+"""
+# CODE
